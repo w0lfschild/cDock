ad64ffb47980d09882ee6f0527e56b6b168f4b94
Wolfgang Baird <w0lf@w0lfbook.local>
2015-07-26 22:51:42 -0700

    cleaned out unneeded paths

--- app/functions/simbl.sh
+++ app/functions/simbl.sh
@@ -1,22 +1,12 @@
 #! /bin/bash
 
 simbl_setup() {
+  lib_plug="/Library/Application Support/SIMBL/Plugins"
   if [[ ! -e /Library/ScriptingAdditions/SIMBL.osax || ! -e /Library/LaunchAgents/net.culater.SIMBL.Agent.plist ]]; then
-    lib_plug="/Library/Application Support/SIMBL/Plugins"
-    usr_plug="$HOME/Library/Application Support/SIMBL/Plugins"
-
     open "$simbl_inst"
-    imma_let_you_finish
-
-    dir_check "$lib_plug"
-    dir_check "$usr_plug"
-    if [[ $(readlink "$usr_plug") != "$lib_plug" ]]; then
-      mv "$usr_plug" "$usr_plug.tmp"
-      ln -s "$lib_plug" "$usr_plug"
-      mv "$usr_plug.tmp/"* "$usr_plug"
-      rm -r "$usr_plug.tmp"
-    fi
+    imma_let_you_finish    
   fi
+  dir_check "$lib_plug"
 }
 
 imma_let_you_finish() {
