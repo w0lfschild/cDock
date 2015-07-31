1dfc319246647a7cb0ba5e199ec84fb98ed89a29
Wolfgang Baird <w0lf@w0lfbook.local>
2015-07-31 14:45:30 -0700

    New SIMBL installer
    - Use cDock-Helper instead of Installer
    - Install to and check /System/Library instead of /Library

--- app/functions/simbl.sh
+++ app/functions/simbl.sh
@@ -2,7 +2,7 @@
 
 simbl_setup() {
   lib_plug="/Library/Application Support/SIMBL/Plugins"
-  if [[ ! -e /Library/ScriptingAdditions/SIMBL.osax || ! -e /Library/LaunchAgents/net.culater.SIMBL.Agent.plist ]]; then
+  if [[ ! -e /System/Library/ScriptingAdditions/SIMBL.osax || ! -e /System/Library/LaunchAgents/net.culater.SIMBL.Agent.plist ]]; then
     open "$simbl_inst"
     imma_let_you_finish    
   fi
@@ -11,20 +11,21 @@ simbl_setup() {
 
 imma_let_you_finish() {
   # echo "But Beyonce had the greatest albumn of all time"
-  inst_id1=$(ps ax | grep [C]oreServices/Installer | sed -e 's/^[ \t]*//' | cut -f1 -d" ")
+  inst_id1=$(ps ax | grep [c]Dock-Helper | sed -e 's/^[ \t]*//' | cut -f1 -d" ")
 
   # If dock has been restarted it will have a new ID
   while [[ $inst_id1 != "" ]]; do
-    inst_id1=$(ps ax | grep [C]oreServices/Installer | sed -e 's/^[ \t]*//' | cut -f1 -d" ")
+    inst_id1=$(ps ax | grep [c]Dock-Helper | sed -e 's/^[ \t]*//' | cut -f1 -d" ")
     sleep .5
   done
 }
 
 simbl_run() {
-  exec "$injec_path" &
-  sleep 1
 	simbl_id=$(ps ax | grep [M]acOS/SIMBL | sed -e 's/^[ \t]*//' | cut -f1 -d" ")
 	if [[ -z $simbl_id ]]; then
-		exec "/Library/ScriptingAdditions/SIMBL.osax/Contents/Resources/SIMBL Agent.app/Contents/MacOS/SIMBL Agent" &
+		exec "/System/Library/ScriptingAdditions/SIMBL.osax/Contents/Resources/SIMBL Agent.app/Contents/MacOS/SIMBL Agent" &
 	fi
+  sleep 1
+  osascript -e 'tell application "Dock" to inject SIMBL into Snow Leopard'
+  osascript -e 'tell application "Finder" to inject SIMBL into Snow Leopard'
 }
