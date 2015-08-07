a8b9782b8c546540950c995902fe04f8c44cf1c5
Wolfgang Baird <w0lf@w0lfbook.local>
2015-08-06 22:02:31 -0700

    More robust attempt at injecting into process

--- app/functions/simbl.sh
+++ app/functions/simbl.sh
@@ -18,12 +18,28 @@ imma_let_you_finish() {
   done
 }
 
+inject_intoPROC() {
+  # Try injecting into process once every 3 seconds for 60 seconds max
+  count=0
+  while [[ $count < 20 ]]; do
+    if [[ $(killall -s "$1") = *"-TERM"* ]]; then
+      count=20
+    fi
+    count=$(( count + 1 ))
+    if [[ $count < 20 ]]; then
+      sleep 3
+    fi
+  done
+  killall -s "$1" && osascript -e "tell application \"$1\" to inject SIMBL into Snow Leopard"
+}
+
 simbl_run() {
+  # Make sure SIMBL is running then try injecting
 	simbl_id=$(ps ax | grep [M]acOS/SIMBL | sed -e 's/^[ \t]*//' | cut -f1 -d" ")
 	if [[ -z $simbl_id ]]; then
 		exec "/System/Library/ScriptingAdditions/SIMBL.osax/Contents/Resources/SIMBL Agent.app/Contents/MacOS/SIMBL Agent" &
 	fi
   sleep 1
-  osascript -e 'tell application "Dock" to inject SIMBL into Snow Leopard'
-  osascript -e 'tell application "Finder" to inject SIMBL into Snow Leopard'
+  inject_intoPROC "Dock" &
+  inject_intoPROC "Finder" &
 }
